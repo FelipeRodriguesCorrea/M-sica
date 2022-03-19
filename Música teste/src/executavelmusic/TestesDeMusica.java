4be3c891a403b07f7ca7caee476ed04f4670fcf4
felip <felip@SR-R0DRIGUES>
2022-03-19 18:34:51 -0300

    Testes do curso

---
+++ Música teste/src/executavelmusic/TestesDeMusica.java
@@ -0,0 +1,13 @@
+package executavelmusic;
+
+public class TestesDeMusica {
+	
+	/*Main é um metodo auto executavel em Java, control+espaço*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no Console*/
+		
+		System.out.println("Meu primeiro Programa em Java");
+	}
+
+}
