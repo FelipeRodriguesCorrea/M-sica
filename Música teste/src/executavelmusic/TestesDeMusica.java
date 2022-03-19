f0b838a8ae99aee22975f8ed8bc842e6b15bb9dc
felip <felip@SR-R0DRIGUES>
2022-03-19 19:23:48 -0300

    Revisão 1

--- Música teste/src/executavelmusic/TestesDeMusica.java
+++ Música teste/src/executavelmusic/TestesDeMusica.java
@@ -5,9 +5,10 @@ public class TestesDeMusica {
 	/*Main é um metodo auto executavel em Java, control+espaço*/
 	public static void main(String[] args) {
 		
-		/*Escreve texto no Console*/
+		/*Escreve texto no Console 0*/
 		
 		System.out.println("Meu primeiro Programa em Java");
+		System.out.println("Escrevendo música");
 	}
 
 }
