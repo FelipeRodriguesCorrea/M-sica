2e1ee40404e90bb8ae33968fc4969a83be9c58a7
felip <felip@SR-R0DRIGUES>
2022-03-19 23:09:58 -0300

    Revert "Adicionei "Aprendendo""
    This reverts commit e8ef9a2d05c12d3b1bf56357b306ed836f96ae25.

--- Música teste/src/executavelmusic/TestesDeMusica.java
+++ Música teste/src/executavelmusic/TestesDeMusica.java
@@ -10,7 +10,6 @@ public class TestesDeMusica {
 		System.out.println("Meu primeiro Programa em Java");
 		System.out.println("Escrevendo música");
 		System.out.println("kkkkk ");
-		System.out.println("Aprendendo");
 		
 	}
 
