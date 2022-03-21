cad3d1a6ceedfbd5a4af85f08ee1dc2b897ad505
felip <felip@SR-R0DRIGUES>
2022-03-21 00:33:14 -0300

    Modulo 7

---
+++ Módulo 7 do Curso/src/TiposVariaveis/Variaveis.java
@@ -0,0 +1,21 @@
+package TiposVariaveis;
+
+public class Variaveis {
+	
+
+	public static void main(String[] args) {
+		
+		boolean logica;
+		boolean logica2;
+		
+		int maiorIdade = 18;
+		int idadeIdoso = 60;
+		int idadeJovem = 16;
+		int idadeCriança = 7;
+		
+		String cpf = "131.648.917-50";
+		
+		System.out.println("Maior idade é = " + maiorIdade);
+		System.out.println("Aposentado é = " + idadeIdoso);
+	}
+}
